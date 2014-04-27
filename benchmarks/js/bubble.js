6ea927afbbbf9c61e3043e9a34cad119ca8c24fd
Erick Lavoie <github@ericklavoie.com>
2014-04-26 21:09:09 -0400

    Added hand written js version of bubble sort

---
+++ benchmarks/js/bubble.js
@@ -0,0 +1,12 @@
+function bubbleJS(A) {
+    var n = A.length;
+    for (var j = 0; j < n-1; ++j) {
+        for (var i = 0; i < n-1; ++i) {
+            if (A[i] > A[i+1]) {
+                var temp = A[i];
+                A[i] = A[i+1];
+                A[i+1] = temp;
+            }
+        }
+    }
+}
